db7c4b08a8d305c239d4a6fe7b91612717f9a354
Henri Karhatsu <henri@karhatsu.com>
2011-02-16 20:56:07 +0200

    exe building without tar2rubyscript

--- build_exe.bat
+++ build_exe.bat
@@ -1,13 +1,11 @@
 @ECHO off
 rem Builds hirviurheilu.zip file containing the exe file and the SQLite3 database file.
 rem Dependencies:
-rem - tar2rubyscript: https://github.com/ryanbooker/tar2rubyscript
 rem - ocra: gem install ocra
 rem - 7za.exe: http://7-zip.org/download.html (the command line version)
 
 @ECHO on
 CALL cd ..
-CALL tar2rubyscript elk_sports
-CALL ocra elk_sports.rb --add-all-core --gemfile elk_sports\Gemfile --dll sqlite3.dll --output hirviurheilu.exe --icon elk_sports\public\favicon.ico
+CALL ocra elk_sports\start.rb elk_sports --no-dep-run --add-all-core --gemfile elk_sports\Gemfile --dll sqlite3.dll --output hirviurheilu.exe --icon elk_sports\public\favicon.ico -- server
 CALL 7za a hirviurheilu.zip hirviurheilu.exe hirviurheilu.sqlite3
 CALL cd elk_sports
---
+++ start.rb
@@ -0,0 +1,3 @@
+p "KAYNNISTETAAN HIRVIURHEILU..."
+Dir.chdir(File.dirname(__FILE__))
+load 'script/rails'
