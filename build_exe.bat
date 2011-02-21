75b466b482757d47c7731920aab3fe1009c209a9
Henri Karhatsu <henri@karhatsu.com>
2011-02-21 20:35:06 +0200

    build exe creates one installation and one upgrade file

--- build_exe.bat
+++ build_exe.bat
@@ -1,5 +1,5 @@
 @ECHO off
-rem Builds hirviurheilu.zip file containing the exe file and the SQLite3 database file.
+rem Builds two hirviurheilu.zip files containing the exe file and the SQLite3 database file (only in the second zip).
 rem Dependencies:
 rem - ocra: gem install ocra
 rem - 7za.exe: http://7-zip.org/download.html (the command line version)
@@ -7,5 +7,6 @@ rem - 7za.exe: http://7-zip.org/download.html (the command line version)
 @ECHO on
 CALL cd ..
 CALL ocra elk_sports\start.rb elk_sports --no-dep-run --add-all-core --gemfile elk_sports\Gemfile --dll sqlite3.dll --output hirviurheilu.exe --icon elk_sports\public\favicon.ico -- server
-CALL 7za a hirviurheilu.zip hirviurheilu.exe hirviurheilu.sqlite3
+CALL 7za a hirviurheilu-asennus.zip hirviurheilu.exe hirviurheilu.sqlite3
+CALL 7za a hirviurheilu-paivitys.zip hirviurheilu.exe
 CALL cd elk_sports
