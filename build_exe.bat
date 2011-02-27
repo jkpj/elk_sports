0e23e05775cab73bc11a127aabb51738cc92a9c7
Henri Karhatsu <henri@karhatsu.com>
2011-02-27 15:10:56 +0200

    production database reset during winoffline building

--- build_exe.bat
+++ build_exe.bat
@@ -5,6 +5,9 @@ rem - ocra: gem install ocra
 rem - 7za.exe: http://7-zip.org/download.html (the command line version)
 
 @ECHO on
+SET RAILS_ENV=production
+CALL rake elk_sports:offline:create_db
+SET RAILS_ENV=
 CALL cd ..
 CALL ocra elk_sports\start.rb elk_sports --no-dep-run --add-all-core --gemfile elk_sports\Gemfile --dll sqlite3.dll --output hirviurheilu.exe --icon elk_sports\public\favicon.ico -- server
 CALL 7za a hirviurheilu-asennus.zip hirviurheilu.exe hirviurheilu.sqlite3
